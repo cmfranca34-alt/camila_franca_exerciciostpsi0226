b2c38e2dcdab9f89d54d2d872e834da37b0ec503
Camila Franca <cmfranca34@gmail.com>
2026-02-27 22:27:45 +0000

    variaveis

--- aula 1/aula.py
+++ aula 1/aula.py
@@ -1,2 +1,47 @@
+''' 
+comentar varias linhas
+
+'''
+
+#comentar na mesma linha
+
+#output
 print ("Olá, mundo!" )
+
+#Int - a variavel pode ser mudada 
+tel=9 # decimal e é 9
+print(type(tel))
+
+#variavel dinamias alteram de tipo automaticamente
+tel="9" #decimal vale 57 de acordo com o ASCII table, pois é uma string 
+print(type(tel))
+print(tel)
+
+
+#float
+med=2.3
+print(type(med))
+print(med)
+
+#string
+nome="Camila"
+print(type(nome))
+print(nom)
+
+#boolean
+flag=True
+print(type(flag))
+print(flag)
+
+#listas []
+lista=[1, "camila", 1.1,False,9]
+print(type(lista))
+print(lista)
+
+#tuples () sao listas que nao se pode mudar o valor depois de criar , cria seguranca para nao ser trocada
+tupla=(1,3,4,6)
+print(type(tula))
+print(tupla)
+
+
 
