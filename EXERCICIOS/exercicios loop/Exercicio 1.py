332da7a078827b5669c997371d4773eabe39995a
Camila Franca <cmfranca34@gmail.com>
2026-03-13 22:49:14 +0000

    Exercicio 1

--- EXERCICIOS/exercicios loop/Exercicio 1.py
+++ EXERCICIOS/exercicios loop/Exercicio 1.py
@@ -1,4 +1,17 @@
 
 # Crie um algoritmo que mostre os 30 primeiros números ímpares e pares.
 
+pares = []
+impares = [] # lista para guardar os numeros
+
+#loop de repeticao
+for i in range (1, 61): 
+    if i % 2 == 0: # condicional, e a % (modulo) retorna o resto da divisao. Nesse caso se restar 0 é par.
+        pares.append(i) #vai adiconando os numeros pares na lista
+    else:
+        impares.append(i) # append é o metodo; vai adicionando os numeros impares na lista
+
+print(f"Pares: {pares}")
+print(f"[Ímpares: {impares}")
+
 
