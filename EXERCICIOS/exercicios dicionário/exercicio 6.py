cb1fc99e7698fa59a57d5f1f35b692f580346c54
Camila Franca <cmfranca34@gmail.com>
2026-04-07 22:10:36 +0100

    feito

--- EXERCICIOS/exercicios dicionário/exercicio 6.py
+++ EXERCICIOS/exercicios dicionário/exercicio 6.py
@@ -1,8 +1,8 @@
 vendas = {'Janeiro': 1000, 'Fevereiro': 1500, 'Março': 1200}
 
-total = 0  # acumulador
+total = 0  #acumulador
 
-for mes in vendas:        # percorre chaves
-    total += vendas[mes]  # soma valores
+for mes in vendas: #percorre chaves
+    total += vendas[mes]  #soma valores
 
 print("Total:", total)
